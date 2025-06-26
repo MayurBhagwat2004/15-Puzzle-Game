586f8024c028552247638b5b4b66ed7f1f7cffe7
MayurBhagwat2004 <mayurbhagwat100@gmail.com>
2025-06-26 15:56:26 +0530

    Added new button prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject[,] tiles = new GameObject[3,3];
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
         
